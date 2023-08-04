26c9148fffe4481a9a3cf4b3d2310730cd0e1011
Aaron <aaron.hsu86+github@gmail.com>
2023-08-03 22:20:13 -0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _6_Dojo_Survey_With_Model.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
